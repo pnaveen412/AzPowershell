f87e9d39b01febd9fa6f65184d1f13032f22759c
NIKHIL <nikhil96761@gmail.com>
2022-09-14 12:00:10 +0530

    ResourceGroupCreation

---
+++ resourcegroup.ps1
@@ -0,0 +1,8 @@
+#Creating a Resource Group 
+$ResourceGroupName = "MyResourceGroup"
+$Location = "East US"
+
+Connect-AzAccount 
+$ResourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+$ResourceGroup.ResourceGroupName + "is creared"
